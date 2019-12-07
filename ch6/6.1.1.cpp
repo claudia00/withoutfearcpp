108735f048836b1096eae4627b774379bbe785a1
Kairi <kairi@Kairis-MacBook-Pro.local>
2019-12-06 23:02:49 -0500

    exercise 6.1.1 uploaded

---
+++ ch6/6.1.1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int arr[] = {5, 15, 25, 35, 45, 55, 65, 75};
+for(int i = 0; i < 8; ++i) {
+cout << arr[i] << " ";
+}
+cout<<endl;
+return 0;
+}
